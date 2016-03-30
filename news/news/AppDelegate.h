523bde7ec7a46a04d4e66045fa2547b1c432b89b
Andy <lovenz2015@gmail.com>
2016-03-30 21:27:50 +0800

    初始化

---
+++ news/news/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  news
+//
+//  Created by Andy Zhao on 16/3/30.
+//  Copyright © 2016年 Andy Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ news/news/Classes/News/NewsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewsTableViewController.h
+//  news
+//
+//  Created by Andy Zhao on 16/3/30.
+//  Copyright © 2016年 Andy Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsTableViewController : UITableViewController
+
+@end
